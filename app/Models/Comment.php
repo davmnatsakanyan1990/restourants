c6562ea8ee6203d924dc49318a4671eae77799cb
Davit Mnatsakanyan <Bastianjung8@gmail.com>
2016-10-26 15:06:32 +0400

    comment data is ready

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -4,14 +4,11 @@ namespace App\Models;
 
 use App\User;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Comment extends Model
 {
     public function commentable(){
         return $this->morphTo();
-    }
-    
-    public function admin(){
-        return $this->morphOne('App\Models\Admin');
     }
 }
