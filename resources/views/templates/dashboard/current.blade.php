fa7cde501fd2a0232c69037f70c1160ae957ff5f
Narine Muzhikyan <nmuzhikyan@gmail.com>
2016-11-18 20:44:17 +0400

    changes in some styles

--- resources/views/templates/dashboard/current.blade.php
+++ resources/views/templates/dashboard/current.blade.php
@@ -95,15 +95,15 @@
                     <div class="containerPart elementLeft">
                         <div class="intro" ng-bind="currentRestaurant.intro"></div>
                         <div class="moreInfo">
-                            <div class="introTitle capitalize">cuisine</div>
+                            <div class="introTitle capitalize"><span class="doubleBorder">cuisine</span></div>
                             <div class="introContent" ng-bind="currentRestaurant.cuisins"></div>
                         </div>
                         <div class="moreInfo">
-                            <div class="introTitle capitalize">Type of place</div>
+                            <div class="introTitle capitalize"><span class="doubleBorder">Type of place</span></div>
                             <div class="introContent" ng-bind="currentRestaurant.type"></div>
                         </div>
                         <div class="moreInfo">
-                            <div class="introTitle capitalize">services</div>
+                            <div class="introTitle capitalize"><span class="doubleBorder">services</span></div>
                             <div class="introContent" ng-bind="currentRestaurant.services"></div>
                         </div>
                     </div>
@@ -129,7 +129,7 @@
                             <div class="moreInfoAddress clearElement">
                                 <div class="introTitle capitalize elementLeft floatNone">Working hours:</div>
                                 <div class="clearElement">
-                                    <div class="introContent clearElement elementLeft" ng-repeat="(day,hourr) in hoursPart">
+                                    <div class="introContent hourContent clearElement elementLeft" ng-repeat="(day,hourr) in hoursPart">
                                         <div class="capitalize infoTitle" ng-bind="day"></div>
                                         <div class="infoHour" ng-repeat="hour in hourr" ng-bind="hour"></div>
                                     </div>
