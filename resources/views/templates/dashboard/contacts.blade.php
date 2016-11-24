efe7e35a9117c540e61f8b8e1c951606bb453aba
Narine Muzhikyan <nmuzhikyan@gmail.com>
2016-11-24 19:25:07 +0400

    start contacts page

---
+++ resources/views/templates/dashboard/contacts.blade.php
@@ -0,0 +1,60 @@
+<div class="organizationContent">
+    <div class="contacts">
+        <div class="topImage">
+            <div class="contactText">
+                <div class="contactTitle">Contact Us</div>
+                <div class="contactContent">We Are so happy to get in touch with you so we can talk about the new opportunities.
+                    Quickly maximize timely deliverables for real-time schemas. Dramatically maintain
+                    clicks-and-mortar solutions without functional solutions.</div>
+            </div>
+        </div>
+        <div class="infoPart">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-7">
+
+                    </div>
+                    <div class="col-md-5">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <footer>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-4 col-sm-4 col-xs-12">
+                    <div class="footerText" data-toggle="modal" data-target="#myModalLocation">add location
+                    </div>
+                    <div class="footerText" data-toggle="modal" data-target="#myModalMaster">register owner</div>
+                    <div class="footerText"><a href="#forOrganization">for organization</a></div>
+                </div>
+                <div class="col-md-4 col-sm-4 col-xs-12">
+                    <div class="footerText"><a href="#contacts">contacts</a></div>
+                    <div class="footerText"><a href="#aboutProject">about project</a></div>
+                    <div class="footerText">Noticed a Mistake - let us</div>
+                </div>
+                <div class="col-md-4 col-sm-4 col-xs-12 floating">
+                    <img src="../images/logo.png">
+                    <div class="footerExplane">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.</br> Beatae, corporis. Beatae, corporis.
+                    </div>
+                    <div class="footerSocial">
+                        <i class="fa fa-facebook" aria-hidden="true"></i>
+                        <i class="fa fa-twitter" aria-hidden="true"></i>
+                        <i class="fa fa-youtube" aria-hidden="true"></i>
+                        <i class="fa fa-instagram" aria-hidden="true"></i>
+                        <i class="fa fa-google-plus" aria-hidden="true"></i>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+
+
+
+
+    </footer>
+</div>
