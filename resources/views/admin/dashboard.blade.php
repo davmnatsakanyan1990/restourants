b6bab7124daef849c3ad5486b702fc349b9b76d3
Anushavan96 <karmar1996@gmail.com>
2016-11-30 15:13:23 +0400

    top curent plan block style

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -5,23 +5,22 @@
 @section('content')
     <div class="page-content-wrapper">
         <div class="page-content">
-            <div class="curent-plan-block">
-                <div class="block">
-                    <h2>Your current plan:Free Trial</h2>
-                    <div class="caunter">
-                       <div>
-                           <span class="num-block first-num">0</span>
-                           <span>:</span>
-                           <span class="num-block last-num">0</span>
-                           <span>Days remaing</span>
-                       </div>
+
+            <div class="wholePage">
+                <div class="curent-plan-block">
+                    <div class="block">
+                        <h2>Your current plan:Free Trial</h2>
+                        <div class="caunter">
+                            <div>
+                                <span class="num-block first-num">0</span>
+                                <span>:</span>
+                                <span class="num-block last-num">0</span>
+                                <span>Days remaing</span>
+                            </div>
+                        </div>
                     </div>
+
                 </div>
-
-            </div>
-
-                <div class="row">
-                    <div class="main_content">
                         @if($is_blocked)
                             <div class="alert alert-danger">
                                 Your restaurant was blocked and removed from list. <a href="{{ url('admin/payment/subscribe') }}">Subscribe</a>
@@ -30,45 +29,47 @@
 
 
 
-                        <div class="col-md-9">
-                            <div class="top-line"></div>
-                            <div class="panel custom_panel panel1">
-                                <div class="panel-body">
-                                    <div class="user-block">
-                                        <div class="image-block">
-                                            <img src="img/user-image-1.png" alt="">
+                        <div class="row">
+                            <div class="col-md-9">
+                                <div class="top-line"></div>
+                                <div class="panel custom_panel panel1">
+                                    <div class="panel-body">
+                                        <div class="user-block">
+                                            <div class="image-block">
+                                                <img src="img/user-image-1.png" alt="">
+                                            </div>
+                                            <div class="text-block">
+                                                <h3>Account : {{ Auth::guard('admin')->user()->name }}</h3>
+                                                <a href="">
+                                                    <i class="fa fa-cog" aria-hidden="true"></i>
+                                                    Account Setting
+                                                </a>
+                                            </div>
                                         </div>
-                                        <div class="text-block">
-                                            <h3>Account : {{ Auth::guard('admin')->user()->name }}</h3>
-                                            <a href="">
-                                                <i class="fa fa-cog" aria-hidden="true"></i>
-                                                Account Setting
-                                            </a>
-                                        </div>
+                                        <h4>Your restaurants</h4>
+                                        <table>
+                                            <tr>
+                                                <td><span class="rest_name">{{ $place->name }}</span></td>
+                                                <td></td>
+                                                <td><a href="{{ url('admin/place/edit') }}">Edit</a></td>
+                                            </tr>
+                                        </table>
                                     </div>
-                                    <h4>Your restaurants</h4>
-                                    <table>
-                                        <tr>
-                                            <td><span class="rest_name">{{ $place->name }}</span></td>
-                                            <td></td>
-                                            <td><a href="{{ url('admin/place/edit') }}">Edit</a></td>
-                                        </tr>
-                                    </table>
                                 </div>
                             </div>
-                        </div>
-                        <div class="col-md-3">
-                            <div class="top-line"></div>
-                            <div class="panel custom_panel panel2 right-panel">
-                                <div class="panel-body">
-                                    <h3>Primary Contact</h3>
-                                    <p><a href="{{ url('admin/profile/edit') }}">Update account details</a></p>
-                                    <p><a href="#">Update billing details</a></p>
+
+                            <div class="col-md-3">
+                                <div class="top-line"></div>
+                                <div class="panel custom_panel panel2 right-panel">
+                                    <div class="panel-body">
+                                        <h3>Primary Contact</h3>
+                                        <p><a href="{{ url('admin/profile/edit') }}">Update account details</a></p>
+                                        <p><a href="#">Update billing details</a></p>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
             </div>
 
     </div>
