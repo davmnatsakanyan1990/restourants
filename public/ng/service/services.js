f5ed38f8c96ce796739f38ec545477496a4f2ee7
Narine Muzhikyan <nmuzhikyan@gmail.com>
2016-11-30 17:52:36 +0400

    improvements for search

--- public/ng/service/services.js
+++ public/ng/service/services.js
@@ -87,7 +87,7 @@ app.factory('RestaurantService', function($http) {
         SearchRestaurant: function(data){
             return $http ({
                 method: "GET",
-                url : "search?=" + JSON.stringify(data)
+                url : "search?cityName=" + data.city + "?value=" + data.restaurant
             })
         }
 
