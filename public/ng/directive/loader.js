e0c0542db66c5edceffd9e4647c58b115f05c756
Narine Muzhikyan <nmuzhikyan@gmail.com>
2016-12-01 11:47:14 +0400

    improvements for scroll

--- public/ng/directive/loader.js
+++ public/ng/directive/loader.js
@@ -2,7 +2,7 @@ app.directive('loading', function () {
     return {
         restrict: 'E',
         replace:true,
-        template: '<div class="loading"><img src="http://www.nasa.gov/multimedia/videogallery/ajax-loader.gif" width="20" height="20" />Loading...</div>',
+        template: '<div class="loading"><img src="images/loader.gif" width="20" height="20" /></div>',
         link: function (scope, element, attr) {
             scope.$watch('loading', function (val) {
                 if (val)
