6471713cb2ac0340efd7af6eb4c7e71fe07d46d2
Narine Muzhikyan <nmuzhikyan@gmail.com>
2016-12-02 12:28:15 +0400

    improvemnts in login registration process

--- public/ng/service/services.js
+++ public/ng/service/services.js
@@ -87,7 +87,7 @@ app.factory('RestaurantService', function($http) {
         SearchRestaurant: function(data){
             return $http ({
                 method: "GET",
-                url : "search?cityName=" + data.city + "?value=" + data.restaurant
+                url : "search?id=" + data.city + "?value=" + data.restaurant
             })
         },
         rootData: function(){
