bc8fbc95eb36045ac5d2021a3f28d2346ea0f10f
Narine Muzhikyan <nmuzhikyan@gmail.com>
2016-12-07 17:40:28 +0400

    improvements in slider styles

--- resources/views/templates/dashboard/current.blade.php
+++ resources/views/templates/dashboard/current.blade.php
@@ -22,17 +22,17 @@
             <span></span>
         </div>
     </nav>
-
     <div id="carousel-id" class="carousel slide carus1" data-ride="carousel">
+
         <div class="carousel-inner">
             <div class="item">
-                <img data-src="holder.js/900x500/auto/#777:#7a7a7a/text:First slide" alt="First slide" src="../images/restaurantSlider/sliderImg1.jpg">
+                <img {{--data-src="holder.js/900x500/auto/#777:#7a7a7a/text:First slide"--}} alt="First slide" ng-src="<% aboutPhoto[0] %>">
             </div>
             <div class="item">
-                <img data-src="holder.js/900x500/auto/#666:#6a6a6a/text:Second slide" alt="Second slide"  src="../images/restaurantSlider/sliderImg2.jpg">
+                <img {{--data-src="holder.js/900x500/auto/#666:#6a6a6a/text:Second slide"--}} alt="Second slide"  ng-src="<% aboutPhoto[1] %>">
             </div>
             <div class="item active">
-                <img data-src="holder.js/900x500/auto/#555:#5a5a5a/text:Third slide" alt="Third slide" src="../images/restaurantSlider/sliderImg3.jpg">
+                <img {{--data-src="holder.js/900x500/auto/#555:#5a5a5a/text:Third slide"--}} alt="Third slide" ng-src="<% aboutPhoto[2] %>">
             </div>
         </div>
         <a class="left carousel-control" href=".carus1" data-slide="prev">
