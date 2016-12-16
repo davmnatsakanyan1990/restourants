f798d839f5dcfca4c089a529e52e7df848bd7277
Narine Muzhikyan <nmuzhikyan@gmail.com>
2016-12-16 10:51:08 +0400

    improvements

--- public/ng/directive/validationDirective.js
+++ public/ng/directive/validationDirective.js
@@ -8,9 +8,12 @@ app.directive('validation', [ 'validationService',function (validationService) {
             required: '=required'
         },
         replace:true,
-        template: '<div class="tooltips" ng-class="{'+'displayNone'+': error.value && !error.pattern}"  style="top: 23px; " ng-bind="error.pattern"></div>',
+        template: '<div class="tooltips" ng-class="{'+'displayNone'+': error.value && !error.pattern}" ng-show="!error.value && error.pattern"  style="top: 23px; " ng-bind="error.pattern"></div>',
         link: function ($scope, element, attr) {
             $scope.$watch('validateValue', function(newValue, oldValue) {
+                if($('.tooltips').hasClass('displayNone')){
+                    $('.tooltips').removeClass('displayNone')
+                }
                 $scope.error= {};
                 $scope.error.value = true;
                 if (newValue) {
@@ -18,12 +21,10 @@ app.directive('validation', [ 'validationService',function (validationService) {
                         var form =attr.validateForm;
                         var forElement = attr.validateFor;
                         $scope.error = validationService.validate(attr.validateFor, $scope.validateValue, attr.required?true:false);
-                        if($('.tooltips').hasClass('displayNone')){
-                            $('.tooltips').removeClass('displayNone')
-                        }
                     }
                 }else if(newValue ==''){
                     $scope.error = validationService.validate(attr.validateFor, $scope.validateValue, attr.required?true:false);
+
                 }
             });
 
