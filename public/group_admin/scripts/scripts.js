5e49798252f46ed88f32cb7c5114a8ebcfab6b89
Narine Muzhikyan <nmuzhikyan@gmail.com>
2017-01-09 18:04:43 +0400

    improvements

--- public/group_admin/scripts/scripts.js
+++ public/group_admin/scripts/scripts.js
@@ -1,5 +1,13 @@
 $( document ).ready(function() {
     $( ".more" ).click(function(e) {
+        var myElement = e.target
+        if($(myElement).hasClass('fa-chevron-down')){
+            $(myElement).removeClass('fa-chevron-down');
+            $(myElement).addClass('fa-chevron-up');
+        }else if($(myElement).hasClass('fa-chevron-up')){
+            $(myElement).removeClass('fa-chevron-up');
+            $(myElement).addClass('fa-chevron-down');
+        }
         var element = e.target.parentNode.parentNode;
         var currentElement = $(element).find('.comments');
         if($( currentElement ).hasClass('hide')){
