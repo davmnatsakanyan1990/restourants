1edf01ea698e2beb20a81dd0b8c930aee1e88840
Narine Muzhikyan <nmuzhikyan@gmail.com>
2017-01-10 10:56:19 +0400

    improvements in footer

--- resources/views/group_admin/layouts/footer.blade.php
+++ resources/views/group_admin/layouts/footer.blade.php
@@ -1,8 +1,8 @@
 <div class="footer">
 	<ul>
-		<li><a href="{{ url('admin/privacy_policy') }}">Privacy Policy  </a></li>
-		<li><a href="{{ url('admin/terms_of_use') }}">Terms of Use </a></li>
-		<li><a href="{{ url('admin/contact_us') }}">Contact Us</a></li>
+		{{--<li><a href="{{ url('group_admin/privacy_policy') }}">Privacy Policy  </a></li>
+		<li><a href="{{ url('group_admin/terms_of_use') }}">Terms of Use </a></li>
+		<li><a href="{{ url('group_admin/contact_us') }}">Contact Us</a></li>--}}
 	</ul>
     <div class="footer-tools">
 		<span class="go-top">
