7f19c3dbfb98a8c6ae6fff02539f03ce8dcf560a
Narine Muzhikyan <nmuzhikyan@gmail.com>
2017-01-11 12:39:17 +0400

    making disable button functionality

--- public/super_admin/scripts/script.js
+++ public/super_admin/scripts/script.js
@@ -1,26 +1,40 @@
 $( document ).ready(function() {
     $('.loadingDiv').hide();
-
+    $('.changeCover').prop('disabled', true);
+    $( "select" ).change(function() {
+        var currentButton = $(this).closest('.restaurant').find('.changeCover');
+        if($(this).val()){
+            $(this).closest('.restaurant').find('.changeCover').prop('disabled', false);
+            if($(currentButton).hasClass('disable')){
+                $(currentButton).removeClass('disable')
+            }
+        }else{
+            $(currentButton).addClass('disable')
+        }
+    });
     $('.changeCover').on('click', function(){
         var text = $(this).closest('.restaurant').find('select').val();
         var place_id = $(this).data('place_id');
         var load= $(this).closest('.restaurant').find('.loadingDiv');
-        $.ajax({
-            url: BASE_URL+'/super_admin/places/update/'+place_id,
-            type: 'post',
-            data: {
-                admin_id: text
-            },
-            success: function(data){
-                load.show();
-            },
-            complete: function(){
-                setTimeout(function(){
-                    load.hide();
-                }, 1000);
 
+        if(text){
+            $.ajax({
+                url: BASE_URL+'/super_admin/places/update/'+place_id,
+                type: 'post',
+                data: {
+                    admin_id: text
+                },
+                success: function(data){
+                    load.show();
+                },
+                complete: function(){
+                    setTimeout(function(){
+                        load.hide();
+                    }, 1000);
+                }
+            })
         }
-        })
     });
+
 
 });
